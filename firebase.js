2da24baf218be7e9a8f83fa40b359de380530b5b
Aryaveer667766 <aryaveer0121@gmail.com>
2025-07-07 13:04:24 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
@@ -12,7 +11,6 @@ const firebaseConfig = {
   measurementId: "G-58WE41QTTZ"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
